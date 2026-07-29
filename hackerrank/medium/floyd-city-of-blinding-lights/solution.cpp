8920b5d139888657e8aa3f046f3d9ce2f6aeb974
Manajit Roy <132820429+Manjitroy1@users.noreply.github.com>
2026-07-29 22:26:24 +0530

    ✅ Floyd : City of Blinding Lights | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-07-29

---
+++ hackerrank/medium/floyd-city-of-blinding-lights/solution.cpp
@@ -0,0 +1,124 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+void floyd(vector<vector<int>>&mat){
+    int n=mat.size();
+    for(int k=0;k<n;k++){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i==j) continue;
+                if(mat[i][k]>=1e9 || mat[k][j]>=1e9) continue;
+                mat[i][j] = min(mat[i][j], mat[i][k] + mat[k][j]);
+            }
+        }
+    }
+}
+
+int main()
+{
+    string road_nodes_edges_temp;
+    getline(cin, road_nodes_edges_temp);
+
+    vector<string> road_nodes_edges = split(rtrim(road_nodes_edges_temp));
+
+    int road_nodes = stoi(road_nodes_edges[0]);
+    int road_edges = stoi(road_nodes_edges[1]);
+
+    vector<int> road_from(road_edges);
+    vector<int> road_to(road_edges);
+    vector<int> road_weight(road_edges);
+    
+    int n=road_nodes;
+    vector<vector<int>>mat(n,vector<int>(n,1e9));
+    for(int i=0;i<n;i++){
+        mat[i][i]=0;
+    }
+    for (int i = 0; i < road_edges; i++) {
+        
+        
+        string road_from_to_weight_temp;
+        getline(cin, road_from_to_weight_temp);
+
+        vector<string> road_from_to_weight = split(rtrim(road_from_to_weight_temp));
+
+        int road_from_temp = stoi(road_from_to_weight[0]);
+        int road_to_temp = stoi(road_from_to_weight[1]);
+        int road_weight_temp = stoi(road_from_to_weight[2]);
+
+        road_from[i] = road_from_temp;
+        road_to[i] = road_to_temp;
+        road_weight[i] = road_weight_temp;
+        
+        int u = road_from_temp-1;
+        int v = road_to_temp-1;
+        int w = road_weight_temp;
+        mat[u][v]=w;
+    }
+    floyd(mat);
+
+    string q_temp;
+    getline(cin, q_temp);
+
+    int q = stoi(ltrim(rtrim(q_temp)));
+
+    for (int q_itr = 0; q_itr < q; q_itr++) {
+        string first_multiple_input_temp;
+        getline(cin, first_multiple_input_temp);
+
+        vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+
+        int x = stoi(first_multiple_input[0]);
+
+        int y = stoi(first_multiple_input[1]);
+        
+        int ans=mat[x-1][y-1];
+        if(ans>=1e9) cout<<-1<<endl;
+        else{ cout<<ans<<endl;}
+    }
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
