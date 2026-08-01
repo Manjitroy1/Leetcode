17cb0a81f5f4c87191e43e589e40c3480290d27c
Manajit Roy <132820429+Manjitroy1@users.noreply.github.com>
2026-08-01 23:21:25 +0530

    ✅ Palindrome Partitioning II | Hard | C++ | Runtime: 84 ms (59.27%) | Memory: 65.7 MB | 2026-08-01

--- leetcode/hard/0132-palindrome-partitioning-ii/solution.cpp
+++ leetcode/hard/0132-palindrome-partitioning-ii/solution.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
-    bool ispalid(int l, int r, string& s){
-        while(l<r){
-            if(s[l]!=s[r]){
-                return false;
-            }
-            l++;
-            r--;
-        }
-        return true;
-    }
-    int recc(int start,int end, string& s,vector<vector<int>>&dp){
+    // bool ispalid(int l, int r, string& s){
+    //     while(l<r){
+    //         if(s[l]!=s[r]){
+    //             return false;
+    //         }
+    //         l++;
+    //         r--;
+    //     }
+    //     return true;
+    // }
+    int recc(int start,int end, string& s,vector<vector<int>>&dp,vector<vector<bool>>&palind){
         if(start>=s.size()) return 0;
         if(dp[start][end]!=-1) return dp[start][end];
 
         int ans=INT_MAX;
         int curr=0;
         for(int k=start;k<=end;k++){
-            if(ispalid(start,k,s)){
-                curr= 1 + recc(k+1,end,s,dp);
+            if(palind[start][k]){
+                curr= 1 + recc(k+1,end,s,dp,palind);
             }
             ans=min(ans,curr);
         }
@@ -30,6 +30,20 @@ public:
         int end=n-1;
 
         vector<vector<int>>dp(n,vector<int>(n,-1));
-        return recc(start,end,s,dp)-1;
+        vector<vector<bool>>palind(n,vector<bool>(n,false));
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i; j < n; j++) {
+
+                if (s[i] == s[j]) {
+
+                    if (j - i <= 1)
+                        palind[i][j] = true;
+                    else
+                        palind[i][j] = palind[i + 1][j - 1];
+                }
+            }
+        }
+        
+        return recc(start,end,s,dp,palind)-1;
     }
 };
