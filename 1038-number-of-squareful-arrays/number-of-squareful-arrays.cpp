2036351f080c7ede8eba129e215f15cb692db62c
Manajit Roy <132820429+Manjitroy1@users.noreply.github.com>
2026-08-07 15:35:40 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (30.76%) - LeetSync

---
+++ 1038-number-of-squareful-arrays/number-of-squareful-arrays.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    int cnt=0;
+    bool sq(int val){
+        int q= sqrt(val);
+       return q*q==val;
+    }
+    // bool func(vector<int>&nums){
+    //     int n=nums.size();
+    //     for(int i=0;i<n-1;i++){
+    //         if(!sq(nums[i]+nums[i+1])){
+    //             return false;
+    //         }
+    //     }
+    //     return true;
+    // }
+    // int solve(vector<int>& nums) {
+    //     int cnt=0;
+    //     sort(nums.begin(),nums.end());
+    //     do{
+    //         if(func(nums)) cnt++;
+    //     }while(next_permutation(nums.begin(),nums.end()));
+
+    //     return cnt;
+    // }
+    void recc(int curr, vector<int>& nums) {
+    if(curr == nums.size()) {
+        cnt++;
+        return;
+    }
+
+    unordered_set<int> used;
+
+    for(int i = curr; i < nums.size(); i++) {
+
+        if(used.count(nums[i])) continue;
+        used.insert(nums[i]);
+
+        swap(nums[curr], nums[i]);
+
+        if(curr == 0 || sq(nums[curr-1] + nums[curr]))
+            recc(curr + 1, nums);
+
+        swap(nums[curr], nums[i]);
+    }
+}
+
+    int numSquarefulPerms(vector<int>& nums) {
+    //    return solve(nums);
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        recc(0,nums); 
+        return cnt;
+    }
+};
