385529ccd1b64892f626b333a77d3b20883daae3
Manajit Roy <132820429+Manjitroy1@users.noreply.github.com>
2026-08-19 00:00:40 +0530

    ✅ Minimum Number of Increments on Subarrays to Form a Target Array | Hard | C++ | Runtime: 0 ms (100.00%) | Memory: 77 MB | 2026-08-18

--- leetcode/hard/1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array/solution.cpp
+++ leetcode/hard/1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array/solution.cpp
@@ -3,12 +3,11 @@ public:
     int minNumberOperations(vector<int>& target) {
         int ans = target[0];
 
-        for (int i = 1; i < target.size(); i++) {
-            if (target[i] > target[i - 1]) {
-                ans += target[i] - target[i - 1];
+        for (int i=1;i<target.size();i++) {
+            if (target[i]>target[i-1]){
+                ans +=target[i]-target[i-1];
             }
         }
-
         return ans;
     }
 };
