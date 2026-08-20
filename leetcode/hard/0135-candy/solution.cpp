5a1f29a84734381d9e95412888132a57b1a9011e
Manajit Roy <132820429+Manjitroy1@users.noreply.github.com>
2026-08-20 20:28:20 +0530

    ✅ Candy | Hard | C++ | Runtime: 78 ms (5.09%) | Memory: 81.5 MB | 2026-08-20

--- leetcode/hard/0135-candy/solution.cpp
+++ leetcode/hard/0135-candy/solution.cpp
@@ -2,7 +2,9 @@ class Solution {
 public:
     int candy(vector<int>& ratings) {
         int n=ratings.size();
-        vector<int>candy(n,0);
+        // vector<int>candy(n,0);
+        vector<int>candy(n,1);
+
 
         using t=pair<int,int>;
         priority_queue<t,vector<t>,greater<t>>pq;
@@ -12,33 +14,38 @@ public:
         }
 
         while(!pq.empty()){
-            auto node= pq.top();
+
+            auto [v, idx] = pq.top();
             pq.pop();
 
-            int v=node.first;
-            int idx=node.second;
+            if (idx > 0 && ratings[idx - 1] < v) {
+                candy[idx] = max(candy[idx], candy[idx - 1] + 1);
+            }
 
+            if (idx + 1 < n && ratings[idx + 1] < v) {
+                candy[idx] = max(candy[idx], candy[idx + 1] + 1);
+            }
 
-            int l = idx-1 >=0 ? ratings[idx-1]: 1e9;
-            int r = idx+1 <n ? ratings[idx+1]: 1e9;
+        //     int l = idx-1 >=0 ? ratings[idx-1]: 1e9;
+        //     int r = idx+1 <n ? ratings[idx+1]: 1e9;
 
-            int mnrating=min(l,r);
+        //     int mnrating=min(l,r);
 
-            int left = idx-1 >=0 ? candy[idx-1]:0;
-            int right = idx+1 <n ? candy[idx+1]:0;
+        //     int left = idx-1 >=0 ? candy[idx-1]:0;
+        //     int right = idx+1 <n ? candy[idx+1]:0;
 
-           //both small
-           if(mnrating>=v) candy[idx] = 1;
-           else if(l<v && r<v){
-                candy[idx] =max(left,right)+1;
-           }
-           //both big or both equal
-           //one small one equal  or one big one small rating
-           else if(l<v){
-            candy[idx] = left+1;
-           }else{
-            candy[idx]= right +1;
-           }
+        //    //both  big or equal
+        //    if(mnrating>=v) candy[idx] = 1;
+        //    //both small
+        //    else if(l<v && r<v){
+        //         candy[idx] =max(left,right)+1;
+        //    }
+        //    //one small one equal  or one big one small rating
+        //    else if(l<v){
+        //     candy[idx] = left+1;
+        //    }else{
+        //     candy[idx]= right +1;
+        //    }
 
         }
         return accumulate(candy.begin(),candy.end(),0);
