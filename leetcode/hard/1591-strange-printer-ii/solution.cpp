3448c8133519349b5e5bcee1b5c64109b466fbfa
Manajit Roy <132820429+Manjitroy1@users.noreply.github.com>
2026-08-21 16:09:46 +0530

    ✅ Strange Printer II | Hard | C++ | Runtime: 114 ms (12.65%) | Memory: 26.8 MB | 2026-08-21

---
+++ leetcode/hard/1591-strange-printer-ii/solution.cpp
@@ -0,0 +1,90 @@
+class Solution {
+public:
+    struct Node{
+        int mnrow,mnclm,mxrow,mxclm;
+    };
+
+    bool kahn(vector<set<int>>&adj){
+        int n=adj.size();
+        queue<int>q;
+
+        vector<int>indeg(n,0);
+        for(int u=0;u<n;u++){
+            for(int v : adj[u]){
+                indeg[v]++;
+            }
+        }
+
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            if(indeg[i]==0){
+                q.push(i);
+            }
+        }
+        if(q.empty()) return false;
+
+        while(!q.empty()){
+            int u=q.front();
+            q.pop();
+            cnt++;
+
+            for(int v : adj[u]){
+                indeg[v]--;
+                if(indeg[v]==0){
+                    q.push(v);
+                }
+            }
+        }
+        return cnt==n? true:false;
+    }
+
+    bool isPrintable(vector<vector<int>>&grid){
+        int n=grid.size();
+        int m=grid[0].size();
+        
+        map<int,Node>mpp;
+
+        unordered_map<int,int>freq;
+        int cnt=0;  // to assign zero based colorign
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                int clr= grid[i][j];
+                //it is a new color
+                if(!freq.count(clr)) freq[clr]=cnt,cnt++;
+
+                if(mpp.count(clr)){
+                    auto& [mnr,mnc,mxr,mxc]= mpp[clr];
+                    mnr= min(mnr,i),mnc=min(mnc,j);
+                    mxr=max(mxr,i),mxc=max(mxc,j);  
+                }
+                else{
+                    mpp[clr]={i,j,i,j};
+                }
+
+            }
+        }
+        vector<set<int>>adj(cnt);  //the graph
+
+        for(auto& data:mpp){
+            int clr=data.first;
+            auto& [mnrow,mnclm,mxrow,mxclm] = data.second;
+
+            for(int i=mnrow;i<=mxrow;i++){
+                for(int j=mnclm;j<=mxclm;j++){
+                    int newclr= grid[i][j];
+
+                    if(newclr!=clr){ /// clr------>newclr
+                        int u= freq[clr];
+                        int v= freq[newclr];
+                        adj[u].insert(v);
+                    }
+                }
+            }
+        }
+
+        //i have the graph go for kahns algo
+        return kahn(adj);
+        
+    }
+};
