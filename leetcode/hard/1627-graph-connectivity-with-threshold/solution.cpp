a9ed47adba55c291ad055a133eb06c2d68baa83a
Manajit Roy <132820429+Manjitroy1@users.noreply.github.com>
2026-08-22 19:03:20 +0530

    ✅ Graph Connectivity With Threshold | Hard | C++ | Runtime: 2801 ms (5.20%) | Memory: 70.2 MB | 2026-08-22

---
+++ leetcode/hard/1627-graph-connectivity-with-threshold/solution.cpp
@@ -0,0 +1,56 @@
+class DSU{
+public:
+    vector<int>parent,size;
+    DSU(int n){
+        parent.resize(n,0);
+        size.resize(n,1);
+        for(int i=0;i<n;i++){
+            parent[i]=i;
+        }
+    }
+
+    int find(int x){
+        if(parent[x]==x) return x;
+        return parent[x]= find(parent[x]);
+    }
+
+    void unite(int u,int v){
+        int up=find(u);
+        int vp=find(v);
+
+        if(up==vp) return;
+        if(size[up]<size[vp]) swap(up,vp);
+        size[up]+=size[vp];
+        parent[vp]=up;
+    }
+};
+class Solution {
+public:
+    vector<bool> areConnected(int n, int threshold, vector<vector<int>>& queries) {
+        DSU dsu(n+1);
+        int q=queries.size();
+        if(threshold==0) return vector<bool>(q,true);
+
+        vector<bool>ans(q,false);
+        if(threshold>=n) return ans;
+
+        for(int i=1;i<=n;i++){
+            for(int j=i+1;j<=n;j++){
+                int g= __gcd(i,j);
+                if(g>threshold){
+                    dsu.unite(i,j);
+                }
+            }
+        }
+
+        for(int i=0;i<q;i++){
+            int u=queries[i][0];
+            int v=queries[i][1];
+            int up=dsu.find(u);
+            int vp=dsu.find(v);
+            if(up==vp) ans[i]=true;
+        }
+        return ans;
+        
+    }
+};
