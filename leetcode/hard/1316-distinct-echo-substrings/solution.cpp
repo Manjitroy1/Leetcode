ba9cf668ec9ae8125fec6b40e04565aeffad98c5
Manajit Roy <132820429+Manjitroy1@users.noreply.github.com>
2026-08-23 22:16:21 +0530

    ✅ Distinct Echo Substrings | Hard | C++ | Runtime: 1033 ms (21.43%) | Memory: 563.5 MB | 2026-08-23

--- leetcode/hard/1316-distinct-echo-substrings/solution.cpp
+++ leetcode/hard/1316-distinct-echo-substrings/solution.cpp
@@ -3,15 +3,21 @@ public:
     int distinctEchoSubstrings(string text) {
         int n=text.size();
         set<string>stt;
-        for(int i=0;i<n-1;i++){
-            for(int len=1;i+ 2*len<=n;len++){
-                if(text.compare(i,len,text,i+len,len)==0){
-                    stt.insert(text.substr(i,2*len));
+        for(int len=1;len<=(n/2);len++){
+            int consecutive=0;
+            for(int i=0;i+len<n;i++){
+                if(text[i]==text[i+len]){
+                    consecutive++;
+                }else{
+                    consecutive=0;
+                }
+
+                if(consecutive==len){
+                    string echo = text.substr(i,len);
+                    stt.insert(echo);
+                    consecutive--;
                 }
             }
-        }
-        for(auto& e: stt){
-            cout<<e<<" ";
         }
         return stt.size();
         
