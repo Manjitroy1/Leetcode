3564c962e7d878bdc50fbd4aeeafd4f694d53350
Manajit Roy <132820429+Manjitroy1@users.noreply.github.com>
2026-08-25 20:38:40 +0530

    ✅ Minimum Number of Removals to Make Mountain Array | Hard | C++ | Runtime: 58 ms (63.86%) | Memory: 16.2 MB | 2026-08-25

--- leetcode/hard/1671-minimum-number-of-removals-to-make-mountain-array/solution.cpp
+++ leetcode/hard/1671-minimum-number-of-removals-to-make-mountain-array/solution.cpp
@@ -27,21 +27,29 @@ public:
             }
             next[i]=1+mx;
         }
+        // int ans=0;
+        // int i=1;
+        // while(i<n && nums[i-1]>=nums[i]){
+        //     i++;
+        // }
+        // int j=n-2;
+        // while(j>0 && nums[j+1]>=nums[j]){
+        //     j--;
+        // }
+
+        // if(j<i) return n-1;
+
+        // for(int k=i;k<=j;k++){
+        //     ans=max(ans,prev[k]+next[k]);
+        // }
+        // return n-ans+1;
+
         int ans=0;
-        int i=1;
-        while(i<n && nums[i-1]>=nums[i]){
-            i++;
+        for(int peak=1;peak<n-1;peak++){
+            if(prev[peak]>1 && next[peak]>1){ //prev this a small one and next there is a small one--> potential ans
+                ans= max(ans,prev[peak]+next[peak]-1);
+            }
         }
-        int j=n-2;
-        while(j>0 && nums[j+1]>=nums[j]){
-            j--;
-        }
-
-        if(j<i) return n-1;
-
-        for(int k=i;k<=j;k++){
-            ans=max(ans,prev[k]+next[k]);
-        }
-        return n-ans+1;
+        return n-ans;
     }
 };
