e9b5e8fe5113073962165efa3cf11e2aab2cb1ce
Manajit Roy <132820429+Manjitroy1@users.noreply.github.com>
2026-08-26 19:13:53 +0530

    Time: 40 ms (90.81%) | Memory: 180.3 MB (90.07%) - LeetSync

--- 307-range-sum-query-mutable/range-sum-query-mutable.cpp
+++ 307-range-sum-query-mutable/range-sum-query-mutable.cpp
@@ -1,62 +1,43 @@
-class SGTree{
-public:
-    vector<int>sgt;
-    SGTree(int n){
-        sgt.resize(4*n+1);
-    }
-    void build(int idx,int low,int high,vector<int>&arr){
-        if(low==high){
-            sgt[idx]=arr[low];
-            return ;
-        }
-        int mid= (low+high)>>1;
-
-        build(2*idx + 1, low,  mid,arr);
-        build(2*idx + 2, mid+1,high,arr);
-        sgt[idx] = sgt[2*idx + 1] + sgt[2*idx + 2];
-    }
-    int query(int idx,int low, int high, int l,int r){
-
-        //no overlap. low high l r.  or l r low high
-        if(l>high || r< low) return 0;
-
-        //complete overlap. l low high  r
-        if(l<=low && high <=r) return sgt[idx];
-
-        //partial ovelap
-        int mid= (low+high)>>1;
-        int left= query(2*idx+1, low, mid,l,r);
-        int right = query(2*idx+2, mid+1, high, l,r);
-        return (left+right);
-    }
-    void update(int idx,int low, int  high, int pos,int val){
-        if(low==high){
-            sgt[idx] = val;
-            return;
-        }
-
-        int mid= (low+high)>>1;
-
-        if(pos<=mid) update(2*idx+1,low,mid, pos,val);
-        else update(2*idx+2,mid+1,high,pos,val);
-
-        sgt[idx] = sgt[2*idx+1] + sgt[2*idx + 2];
-    }
-};
 class NumArray {
 public:
     int n;
-    SGTree sg;
-    NumArray(vector<int>& nums) : n(nums.size()), sg(n) {
-        sg.build(0,0,n-1,nums);
+    vector<int>bit;
+    vector<int>nums;
+
+    void upd(int idx,int val){
+        while(idx<=n){
+            bit[idx]+=val;
+            idx+=(idx & -idx);
+        }
+    }
+    int query(int idx){
+        int ans=0;
+        while(idx>0){
+            ans+=bit[idx];
+            idx-=(idx & -idx);
+        }
+        return ans;
+    }
+    NumArray(vector<int>& arr) {
+        this->nums = arr;
+        n=nums.size();
+        bit.resize(n+1,0);
+
+        for(int i=0;i<n;i++){
+            upd(i+1,nums[i]);
+        }
     }
     
     void update(int index, int val) {
-        sg.update(0,0,n-1,index,val);
+        int diff= val-nums[index];
+        nums[index]=val;
+        upd(index+1,diff);
+        
     }
     
     int sumRange(int left, int right) {
-        return sg.query(0,0,n-1,left,right);
+        return query(right+1) - query(left);
+        
     }
 };
 
