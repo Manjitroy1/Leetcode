a9d6bc404872bb7bbbdc1cf85feaabb43a14937a
Manajit Roy <132820429+Manjitroy1@users.noreply.github.com>
2026-08-29 00:38:57 +0530

    ✅ Max Chunks To Make Sorted II | Hard | C++ | Runtime: 8 ms (21.35%) | Memory: 17.9 MB | 2026-08-28

--- leetcode/hard/0768-max-chunks-to-make-sorted-ii/solution.cpp
+++ leetcode/hard/0768-max-chunks-to-make-sorted-ii/solution.cpp
@@ -5,18 +5,22 @@ public:
         vector<int>nums=arr;
         sort(nums.begin(),nums.end());
         if(nums==arr) return n;
-        map<int,int>stt1;
-        map<int,int>stt2;
+        map<int,int>freq;
+        // map<int,int>stt2;
 
         int cnt=0;
         for(int i=0;i<n;i++){
-            stt1[arr[i]]++;
-            stt2[nums[i]]++;
+            freq[arr[i]]++;
+            freq[nums[i]]--;
 
-            if(stt1==stt2){
+            if(freq[arr[i]]==0){
+                freq.erase(arr[i]);
+            }
+            if(freq[nums[i]]==0){
+                freq.erase(nums[i]);
+            }
+            if(freq.empty()){
                 cnt++;
-                stt1.clear();
-                stt2.clear();
             }
         }
         return cnt;
