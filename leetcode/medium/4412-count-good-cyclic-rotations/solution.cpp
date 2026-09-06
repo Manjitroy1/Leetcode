0061d2fbe0568c1aadc40e2dad2ee700fa60b791
Manajit Roy <132820429+Manjitroy1@users.noreply.github.com>
2026-09-06 16:30:15 +0530

    ✅ Q2. Count Good Cyclic Rotations | Medium | C++ | Runtime: 7 ms (36.86%) | Memory: 103.3 MB | 2026-09-06

--- leetcode/medium/4412-count-good-cyclic-rotations/solution.cpp
+++ leetcode/medium/4412-count-good-cyclic-rotations/solution.cpp
@@ -1,29 +1,49 @@
 #define ll long long
 class Solution {
 public:
+    // int brute(vector<int>& nums) {
+    //     int n=nums.size();
+    //     int m=2*n;
+        
+    //     ll pref=0;
+    //     ll suff=0;
+        
+    //     for(int i=0;i<n;i++){
+    //         if(i<n/2) pref+=nums[i];
+    //         else suff+=nums[i];
+    //     }
+    //     int cnt=0;
+    //     for(int i=0;i<n;i++){
+            
+    //         if(pref>suff) cnt++;
+            
+    //         int j=(i+n)%n;
+    //         int k=(i+(n/2))%n;
+            
+    //         pref-=nums[i];
+    //         pref+=nums[k];
+    //         suff-=nums[k];
+    //         suff+=nums[j];   
+    //     }
+    //     return cnt;
+    // }
     int countGoodRotations(vector<int>& nums) {
         int n=nums.size();
-        int m=2*n;
-        
         ll pref=0;
-        ll suff=0;
+        ll total=0;
+        int len=n/2; //half
         
         for(int i=0;i<n;i++){
-            if(i<n/2) pref+=nums[i];
-            else suff+=nums[i];
+            total+=nums[i];
+            if(i<len) pref+=nums[i];
         }
         int cnt=0;
         for(int i=0;i<n;i++){
-            
-            if(pref>suff) cnt++;
-            
-            int j=(i+n)%n;
-            int k=(i+(n/2))%n;
-            
+            if(2*pref>total){
+                cnt++;
+            }
             pref-=nums[i];
-            pref+=nums[k];
-            suff-=nums[k];
-            suff+=nums[j];   
+            pref+=nums[(i+len) % n];
         }
         return cnt;
     }
