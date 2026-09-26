fd708c59a1311764e7371dc647a7c96fb0e801a9
Manajit Roy <132820429+Manjitroy1@users.noreply.github.com>
2026-09-26 16:28:24 +0530

    Time: 91 ms (97.13%) | Memory: 121.8 MB (44.06%) - LeetSync

--- 2375-minimum-obstacle-removal-to-reach-corner/minimum-obstacle-removal-to-reach-corner.cpp
+++ 2375-minimum-obstacle-removal-to-reach-corner/minimum-obstacle-removal-to-reach-corner.cpp
@@ -1,5 +1,46 @@
 class Solution {
 public:
+    int dijk01(vector<vector<int>>& grid){
+        int n=grid.size();
+        int m=grid[0].size();
+        using t=pair<int,int>; //r,c
+        vector<vector<int>>dis(n,vector<int>(m,n*m));
+
+        vector<pair<int,int>>dir={{0,1},{0,-1},{1,0},{-1,0}};
+
+        //we will use dequeue
+        deque<t>q; //r c
+        dis[0][0]=0;
+        q.push_front({0,0});
+
+        while(!q.empty()){
+            auto [r,c] = q.front();
+            q.pop_front();
+
+            if(r==n-1 && c==m-1) return dis[r][c];
+
+            for(auto d:dir){
+                int vr=r+d.first;
+                int vc=c+d.second;
+
+                if(vr>=0 && vr<n && vc>=0 && vc<m){
+                    int b=grid[vr][vc]? 1:0;
+                    int newblock = dis[r][c]+b;
+
+                    if(dis[vr][vc] > newblock){
+                        dis[vr][vc]=newblock;
+                        if(b){
+                            q.push_back({vr,vc});
+                        }else{
+                            q.push_front({vr,vc});
+                        }
+                    }
+                }
+            }
+        }
+        return dis[n-1][m-1];
+
+    }
     int dijkstra(vector<vector<int>>& grid){
         int n=grid.size();
         int m=grid[0].size();
@@ -36,7 +77,8 @@ public:
         return dis[n-1][m-1];
     }
     int minimumObstacles(vector<vector<int>>& grid) {
-        int ans=dijkstra(grid);
+        // int ans=dijkstra(grid);
+        int ans=dijk01(grid);
         return ans;
     }
 };
