b1574fbc79c5ae054017b9a6a1b1446ec94ea260
Manajit Roy <132820429+Manjitroy1@users.noreply.github.com>
2026-09-26 21:28:21 +0530

    ✅ Q2. Transform Array Using Pair Operations | Medium | C++ | Runtime: 0 ms (100.00%) | Memory: 193.3 MB | 2026-09-26

---
+++ leetcode/medium/4398-transform-array-using-pair-operations/solution.cpp
@@ -0,0 +1,14 @@
+#define ll long long
+class Solution {
+public:
+    bool canTransform(vector<int>& source, vector<int>& target) {
+        // ll s1=accumulate(source.begin(),source.end(),0);
+        // ll s2=accumulate(target.begin(),target.end(),0);
+        ll s1=0,s2=0;
+        for(int& x:source) s1+=x;
+        for(int& x:target) s2+=x;
+
+        return s1==s2;
+        
+    }
+};
